6d84d822543ddb8765970374415dbc3df0f5c8c5
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-04-28 16:14:02 -0300

    hardcode mongouri

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI')
+//const db = config.get('mongoURI')
+const db = "mongodb+srv://admin:admin@trekkingbsas.pjsax.mongodb.net/TrekkingBsAs?retryWrites=true&w=majority";
 
 const connectDB = async () => {
     try {
         await mongoose.connect(db);
 
         console.log('MongoDb Connected...')
-    } catch(err) {
+    } catch (err) {
         console.error(err);
         // Exit process with failure
         process.exit(1)
